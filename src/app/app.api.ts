65090bf2fef00ac61cf2bdb7518a6c4f131458f1
Ludmyla Soares <ludmylaoliveirasoares@gmail.com>
2020-04-14 22:34:09 -0300

    Aplicação Funcionando - Ok

--- src/app/app.api.ts
+++ src/app/app.api.ts
@@ -1,4 +1,4 @@
-export const URL_API = 'https://jta-passaro-urbano.firebaseio.com'
+export const URL_API = 'https://my-json-server.typicode.com/ludmylasoares/json-server'
  
 // URL_API = 'http://localhost:3000'; // api rest fake POST funciona localmente
 // URL_API = 'https://my-json-server.typicode.com/ludmylasoares/json-server'; // api rest fake online POST não funciona
